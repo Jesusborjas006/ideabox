42edbfd560cc71066ea1da12f6d4c9ba0e7d4b18
Jesusborjas006 <jesusbor006@gmail.com>
2024-06-13 18:07:42 -0500

    Create button in nan

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,7 +1,10 @@
 const SideBar = () => {
   return (
-    <nav className="sticky top-0 bg-[#1f1f3C] text-white min-h-screen">
-      <h1 className="text-5xl font-bold pt-10 px-8">IdeaBox</h1>
+    <nav className="sticky top-0 bg-[#1f1f3C] text-white min-h-screen px-10">
+      <h1 className="text-5xl font-bold pt-10 tracking-wider">IdeaBox</h1>
+      <button className="bg-[#eaeaf4] text-[#1f1f3C] text-lg font-bold w-full mt-10 py-2">
+        Show Starred Ideas
+      </button>
     </nav>
   );
 };
