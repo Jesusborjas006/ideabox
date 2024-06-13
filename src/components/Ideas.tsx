f22dcd6aa292b9bc2eaaca9a3f46a1bf0dd2edb7
Jesusborjas006 <jesusbor006@gmail.com>
2024-06-13 15:44:30 -0500

    Handle long body content styling by breaking the words into rows

--- src/components/Ideas.tsx
+++ src/components/Ideas.tsx
@@ -8,10 +8,12 @@ const Ideas = ({ ideas }: IdeasProps) => {
   const ideaElements = ideas.map((idea) => (
     <div
       key={idea.id}
-      className="bg-white text-[#1f1f3C] border-2 border-[#1f1f3C] h-[250px] text-center "
+      className="bg-white text-[#1f1f3C] border-2 border-[#1f1f3C] h-[250px]  overflow-scroll"
     >
-      <h3 className="text-2xl font-semibold my-4">{idea.title}</h3>
-      <p className="mt-2">{idea.body}</p>
+      <h3 className="text-2xl font-semibold my-4 ml-6">{idea.title}</h3>
+      <p className="mt-2 border border-red-500 overflow-scroll break-words px-6">
+        {idea.body}
+      </p>
     </div>
   ));
   return (
