fe284c5524ce5766c673f19f5dc23423eca7e485
Jesusborjas006 <jesusbor006@gmail.com>
2024-06-13 18:33:13 -0500

    Toggle between favorited ideas and not favorited

--- src/components/Ideas.tsx
+++ src/components/Ideas.tsx
@@ -6,9 +6,19 @@ interface IdeasProps {
   ideas: [] | IdeaType[];
   deleteIdea: (id: number) => void;
   toggleFavorite: (id: number) => void;
+  showFavorites: boolean;
 }
 
-const Ideas = ({ ideas, deleteIdea, toggleFavorite }: IdeasProps) => {
+const Ideas = ({
+  ideas,
+  deleteIdea,
+  toggleFavorite,
+  showFavorites,
+}: IdeasProps) => {
+  const favoriteIdeas = ideas.filter((idea) => {
+    return idea.isFavorite;
+  });
+
   const ideaElements = ideas.map((idea) => (
     <div
       key={idea.id}
@@ -31,9 +41,35 @@ const Ideas = ({ ideas, deleteIdea, toggleFavorite }: IdeasProps) => {
       <p className="mt-2 overflow-scroll break-words px-6">{idea.body}</p>
     </div>
   ));
+
+  const favoriteIdeaElements = favoriteIdeas.map((idea) => (
+    <div
+      key={idea.id}
+      className="bg-white text-[#1f1f3C] border-2 border-[#1f1f3C] h-[250px]  overflow-scroll"
+    >
+      <div className="bg-[#1f1f3C] text-white flex justify-between px-3 py-1 h-[40px]">
+        <img
+          src={idea.isFavorite ? activeStar : star}
+          alt="Star Symbol"
+          onClick={() => toggleFavorite(idea.id)}
+        />
+        <button
+          className="text-2xl hover:text-red-500"
+          onClick={() => deleteIdea(idea.id)}
+        >
+          X
+        </button>
+      </div>
+      <h3 className="text-2xl font-semibold my-4 ml-6">{idea.title}</h3>
+      <p className="mt-2 overflow-scroll break-words px-6">{idea.body}</p>
+    </div>
+  ));
+
   return (
     <section className="p-10">
-      <div className="grid grid-cols-3 gap-6">{ideaElements}</div>
+      <div className="grid grid-cols-3 gap-6">
+        {showFavorites ? favoriteIdeaElements : ideaElements}
+      </div>
     </section>
   );
 };
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,9 +1,17 @@
-const SideBar = () => {
+interface SideBarProps {
+  showFavorites: boolean;
+  setShowFavorites: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const SideBar = ({ showFavorites, setShowFavorites }: SideBarProps) => {
   return (
     <nav className="sticky top-0 bg-[#1f1f3C] text-white min-h-screen px-10">
       <h1 className="text-5xl font-bold pt-10 tracking-wider">IdeaBox</h1>
-      <button className="bg-[#eaeaf4] text-[#1f1f3C] text-lg font-bold w-full mt-10 py-2">
-        Show Starred Ideas
+      <button
+        className="bg-[#eaeaf4] text-[#1f1f3C] text-lg font-bold w-full mt-10 py-2"
+        onClick={() => setShowFavorites((prev) => !prev)}
+      >
+        {showFavorites ? "Show All Ideas" : "Show Starred Ideas"}
       </button>
     </nav>
   );
