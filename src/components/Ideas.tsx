bb2aab9c5b39ae43bf400a16eff960bbdfb1c4e3
Jesusborjas006 <jesusbor006@gmail.com>
2024-06-13 18:56:26 -0500

    Combine the filter and map together to display favorites

--- src/components/Ideas.tsx
+++ src/components/Ideas.tsx
@@ -26,26 +26,26 @@ const Ideas = ({
     />
   ));
 
-  const favoriteIdeas = ideas.filter((idea) => {
-    return idea.isFavorite;
-  });
-
-  const favoriteIdeaElemens = favoriteIdeas.map((idea) => (
-    <Idea
-      key={idea.id}
-      id={idea.id}
-      title={idea.title}
-      body={idea.body}
-      isFavorite={idea.isFavorite}
-      deleteIdea={deleteIdea}
-      toggleFavorite={toggleFavorite}
-    />
-  ));
+  const favoriteIdeas = ideas
+    .filter((idea) => {
+      return idea.isFavorite;
+    })
+    .map((idea) => (
+      <Idea
+        key={idea.id}
+        id={idea.id}
+        title={idea.title}
+        body={idea.body}
+        isFavorite={idea.isFavorite}
+        deleteIdea={deleteIdea}
+        toggleFavorite={toggleFavorite}
+      />
+    ));
 
   return (
     <section className="p-10">
       <div className="grid grid-cols-3 gap-6">
-        {showFavorites ? favoriteIdeaElemens : ideaElements}
+        {showFavorites ? favoriteIdeas : ideaElements}
       </div>
     </section>
   );
