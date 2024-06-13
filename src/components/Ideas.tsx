f2f26a2354e326a0e7a993a68bfb2a81e42fa5e0
Jesusborjas006 <jesusbor006@gmail.com>
2024-06-13 16:18:21 -0500

    Add functionality to remove an idea from data

--- src/components/Ideas.tsx
+++ src/components/Ideas.tsx
@@ -2,16 +2,22 @@ import { IdeaType } from "../types";
 
 interface IdeasProps {
   ideas: [] | IdeaType[];
+  deleteIdea: (id: number) => void;
 }
 
-const Ideas = ({ ideas }: IdeasProps) => {
+const Ideas = ({ ideas, deleteIdea }: IdeasProps) => {
   const ideaElements = ideas.map((idea) => (
     <div
       key={idea.id}
       className="bg-white text-[#1f1f3C] border-2 border-[#1f1f3C] h-[250px]  overflow-scroll"
     >
       <div className="bg-[#1f1f3C] text-white flex justify-end pr-4 py-1">
-        <p className="text-2xl cursor-pointer hover:text-red-500">X</p>
+        <button
+          className="text-2xl hover:text-red-500"
+          onClick={() => deleteIdea(idea.id)}
+        >
+          X
+        </button>
       </div>
       <h3 className="text-2xl font-semibold my-4 ml-6">{idea.title}</h3>
       <p className="mt-2 overflow-scroll break-words px-6">{idea.body}</p>
