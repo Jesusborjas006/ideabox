8ddbe65c9857f70a78f1e1015ef5b5ce447c54d0
Jesusborjas006 <jesusbor006@gmail.com>
2024-06-13 16:14:29 -0500

    Create delete button for each card and add styling

--- src/components/Ideas.tsx
+++ src/components/Ideas.tsx
@@ -10,10 +10,11 @@ const Ideas = ({ ideas }: IdeasProps) => {
       key={idea.id}
       className="bg-white text-[#1f1f3C] border-2 border-[#1f1f3C] h-[250px]  overflow-scroll"
     >
+      <div className="bg-[#1f1f3C] text-white flex justify-end pr-4 py-1">
+        <p className="text-2xl cursor-pointer hover:text-red-500">X</p>
+      </div>
       <h3 className="text-2xl font-semibold my-4 ml-6">{idea.title}</h3>
-      <p className="mt-2 border border-red-500 overflow-scroll break-words px-6">
-        {idea.body}
-      </p>
+      <p className="mt-2 overflow-scroll break-words px-6">{idea.body}</p>
     </div>
   ));
   return (
