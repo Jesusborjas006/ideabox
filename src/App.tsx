693cbdeb54a4650c4b934c698111458fb88481d1
Jesusborjas006 <jesusbor006@gmail.com>
2024-06-13 16:59:50 -0500

    Create seperate favorites state to store favorated ideas

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import { IdeaType } from "./types";
 
 function App() {
   const [ideas, setIdeas] = useState<[] | IdeaType[]>([]);
+  const [favorites, setFavorites] = useState<[] | IdeaType[]>([]);
+  console.log("All FAvorites: ", favorites);
 
   const addIdea = (newIdea: IdeaType) => {
     setIdeas([...ideas, newIdea]);
@@ -18,12 +20,25 @@ function App() {
     setIdeas(updatedIdeas);
   };
 
+  const addToFavorites = (id: number) => {
+    const favoritedIdea = ideas.find((idea) => {
+      return idea.id === id;
+    });
+    if (favoritedIdea) {
+      setFavorites([...favorites, favoritedIdea]);
+    }
+  };
+
   return (
     <main className="flex">
       <SideBar />
       <section className="w-full h-screen overflow-y-auto">
         <Form addIdea={addIdea} />
-        <Ideas ideas={ideas} deleteIdea={deleteIdea} />
+        <Ideas
+          ideas={ideas}
+          deleteIdea={deleteIdea}
+          addToFavorites={addToFavorites}
+        />
       </section>
     </main>
   );
--- src/components/Ideas.tsx
+++ src/components/Ideas.tsx
@@ -1,17 +1,25 @@
 import { IdeaType } from "../types";
+import star from "../../public/assets/star.svg";
+import activeStar from "../../public/assets/star-active.svg";
 
 interface IdeasProps {
   ideas: [] | IdeaType[];
   deleteIdea: (id: number) => void;
+  addToFavorites: (id: number) => void;
 }
 
-const Ideas = ({ ideas, deleteIdea }: IdeasProps) => {
+const Ideas = ({ ideas, deleteIdea, addToFavorites }: IdeasProps) => {
   const ideaElements = ideas.map((idea) => (
     <div
       key={idea.id}
       className="bg-white text-[#1f1f3C] border-2 border-[#1f1f3C] h-[250px]  overflow-scroll"
     >
-      <div className="bg-[#1f1f3C] text-white flex justify-end pr-4 py-1">
+      <div className="bg-[#1f1f3C] text-white flex justify-between px-3 py-1 h-[40px]">
+        <img
+          src={star}
+          alt="Star Symbol"
+          onClick={() => addToFavorites(idea.id)}
+        />
         <button
           className="text-2xl hover:text-red-500"
           onClick={() => deleteIdea(idea.id)}
