edb3b39a16b5d2d23ae7c76b8262e13a453dded5
Jesusborjas006 <jesusbor006@gmail.com>
2024-06-14 13:39:34 -0500

    Create search input with styling and add state value

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,9 +1,13 @@
+import { useState } from "react";
+
 interface SideBarProps {
   showFavorites: boolean;
   setShowFavorites: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 const SideBar = ({ showFavorites, setShowFavorites }: SideBarProps) => {
+  const [query, setQuery] = useState("");
+
   return (
     <nav className="sticky top-0 bg-[#1f1f3C] text-white min-h-screen px-10">
       <h1 className="text-5xl font-bold pt-10 tracking-wider">IdeaBox</h1>
@@ -13,6 +17,13 @@ const SideBar = ({ showFavorites, setShowFavorites }: SideBarProps) => {
       >
         {showFavorites ? "Show All Ideas" : "Show Starred Ideas"}
       </button>
+      <input
+        className="border-2 mt-10 w-full indent-2 text-black"
+        type="text"
+        placeholder="Search Idea..."
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
     </nav>
   );
 };
